9bd3aa9f8deb53567a745ed0bd1558ae15aa0b67
James Becker <jamsbec@gmail.com>
2017-02-23 11:23:23 +0100

    Create relevant controllers and models

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  belongs_to :ingredient
+  belongs_to :cocktail, dependent: :destroy
+
+  validates :description, presence: true
+  validates_uniqueness_of :ingredient, :scope => [:cocktail]
+end
